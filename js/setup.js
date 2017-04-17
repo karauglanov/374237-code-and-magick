bc928c9de547a0af32de0c1172898252f4a53bdf
Sergey <karauglanov@yandex.ru>
2017-04-17 10:31:35 +0300

    Версия №2

--- js/setup.js
+++ js/setup.js
@@ -19,48 +19,34 @@ var similarListElement = userDialog.querySelector('.setup-similar-list');
 // Находим блок #similar-wizard-template и берём его содержимое
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
-// Функция определения случайного числа
-var randomNumber = function (wizardData) {
-  return Math.floor(Math.random() * wizardData.length);
-};
+// Функция возвращает значения константы (массива) по генератору случайного числа
+function randomItem (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
 
-// Создаём массив состоящий из объектов заполненный из массивов констант, содержащих имена, фамилии, цвет мантии, цвет глаз
-var wizards = [
-  {
-    name: WIZARD_NAMES[randomNumber(WIZARD_NAMES)] + ' ' + WIZARD_SURNAME[randomNumber(WIZARD_SURNAME)], // Присвоение имени и фамилии с помощью функции случайного числа
-    coatColor: WIZARD_COATCOLOR[randomNumber(WIZARD_COATCOLOR)], // Присвоение цвета мантии с помощью функции случайного числа
-    eyesColor: WIZARD_EYESCOLOR[randomNumber(WIZARD_EYESCOLOR)]// Присвоение ывета глаз с помощью функции случайного числа
-  },
-  {
-    name: WIZARD_NAMES[randomNumber(WIZARD_NAMES)] + ' ' + WIZARD_SURNAME[randomNumber(WIZARD_SURNAME)], // Присвоение имени и фамилии с помощью функции случайного числа
-    coatColor: WIZARD_COATCOLOR[randomNumber(WIZARD_COATCOLOR)], // Присвоение цвета мантии с помощью функции случайного числа
-    eyesColor: WIZARD_EYESCOLOR[randomNumber(WIZARD_EYESCOLOR)]// Присвоение ывета глаз с помощью функции случайного числа  },
-  },
-  {
-    name: WIZARD_NAMES[randomNumber(WIZARD_NAMES)] + ' ' + WIZARD_SURNAME[randomNumber(WIZARD_SURNAME)], // Присвоение имени и фамилии с помощью функции случайного числа
-    coatColor: WIZARD_COATCOLOR[randomNumber(WIZARD_COATCOLOR)], // Присвоение цвета мантии с помощью функции случайного числа
-    eyesColor: WIZARD_EYESCOLOR[randomNumber(WIZARD_EYESCOLOR)]// Присвоение ывета глаз с помощью функции случайного числа  },
-  },
-  {
-    name: WIZARD_NAMES[randomNumber(WIZARD_NAMES)] + ' ' + WIZARD_SURNAME[randomNumber(WIZARD_SURNAME)], // Присвоение имени и фамилии с помощью функции случайного числа
-    coatColor: WIZARD_COATCOLOR[randomNumber(WIZARD_COATCOLOR)], // Присвоение цвета мантии с помощью функции случайного числа
-    eyesColor: WIZARD_EYESCOLOR[randomNumber(WIZARD_EYESCOLOR)]// Присвоение ывета глаз с помощью функции случайного числа  },
-  },
-];
+// Функция создания мага со случайными параметрами
+function generateRandomWizard() {
+    var wizard =  {
+      name: randomItem(WIZARD_NAMES) + ' ' + randomItem(WIZARD_SURNAME), // Присвоение имени и фамилии с помощью функции случайного числа
+      coatColor: randomItem(WIZARD_COATCOLOR), // Присвоение цвета мантии с помощью функции случайного числа
+      eyesColor: randomItem(WIZARD_EYESCOLOR) // Присвоение ывета глаз с помощью функции случайного числа
+    };
+  return wizard;
+}
 
 // Функция позволяющая создать нужного нам волшебника с теми параметрами которые мы ей передаём
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);// Клонируем волшебника с теми параметрами которые были в шаблоне #similar-wizard-template
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;// Заменяем шаблонное имя на имя переданное в переменной wizards
-  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;// Заменяем шаблонный цвет мантии на цвет переданный в переменной wizards
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;// Заменяем шаблонный цвет глаз на цвет переданное в переменной wizards
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;// Заменяем шаблонный цвет мантии на цвет переданный в переменной wizards
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;// Заменяем шаблонный цвет глаз на цвет переданное в переменной wizards
   return wizardElement;// Возвращаем из функции значение переменной wizardElement
 };
 
 var fragment = document.createDocumentFragment();// Создаём переменную fragment методом DocumentFragment для того что бы можно было сразу вывести всех магов, а не по одному
 
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));// Добавляем волшебников (детей) в элемент fragment
+for (var i = 0; i < 4; i++) {
+  fragment.appendChild(renderWizard(generateRandomWizard()));// Добавляем волшебников (детей) в элемент fragment
 }
 
 similarListElement.appendChild(fragment);// Передаём данные о волшебниках (детях) в блок .setup-similar-list');};
